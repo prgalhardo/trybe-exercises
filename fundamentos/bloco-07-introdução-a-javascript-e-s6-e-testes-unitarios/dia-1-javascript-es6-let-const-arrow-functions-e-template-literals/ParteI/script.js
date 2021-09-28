861ad70c38032051a78539b06e86d522836e7b08
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-09-27 23:29:03 -0300

    Adiciona novas alteraçoes

--- fundamentos/bloco-07-introdução-a-javascript-e-s6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ParteI/script.js
+++ fundamentos/bloco-07-introdução-a-javascript-e-s6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ParteI/script.js
@@ -28,3 +28,4 @@ console.log(
     ordemCrescente
   )} se encontram ordenados em forma crescente.`
 );
+
--- fundamentos/bloco-07-introdução-a-javascript-e-s6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ParteII/script.js
+++ fundamentos/bloco-07-introdução-a-javascript-e-s6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ParteII/script.js
@@ -30,3 +30,11 @@ console.log(
 // Um botão ao qual será associado um event listener ;
 // Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
 // Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+
+const fullName = (nomeCompleto, nome, string) => {
+  nomeCompleto = {
+  nome: 'Priscila',
+  };
+
+}
+console.log(fullName(nomeCompleto, nome, string));
