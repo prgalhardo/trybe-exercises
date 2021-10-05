bd7a9738640677f0f8245766fd7efbc9fe241688
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-10-05 00:11:25 -0300

    Adiciona novo exercicio

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.1/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.1/script.js
@@ -1,47 +1,47 @@
-// // Vamos praticar com os seguintes exercícios:
-// // 1 - Crie uma função que retorne a string 'Acordando!!' ;
-// const wakeUp = () => {
-//   return "Acordando!!";
-// };
+// Vamos praticar com os seguintes exercícios:
+// 1 - Crie uma função que retorne a string 'Acordando!!' ;
+const wakeUp = () => {
+  return "Acordando!!";
+};
 
-// // 2 - Crie outra função que retorne a string 'Bora tomar café!!' ;
-// const breakfast = () => {
-//   return "Bora tomar café!!";
-// };
+// 2 - Crie outra função que retorne a string 'Bora tomar café!!' ;
+const breakfast = () => {
+  return "Bora tomar café!!";
+};
 
-// // 3 - Crie mais uma função que retorne a string 'Partiu dormir!!' ;
-// const sleep = () => {
-//   return "Partiu dormir!";
-// };
+// 3 - Crie mais uma função que retorne a string 'Partiu dormir!!' ;
+const sleep = () => {
+  return "Partiu dormir!";
+};
 
-// // 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
-// const doingThings = (callback) => {
-//   console.log(callback());
-// };
+// 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
+const doingThings = (callback) => {
+  console.log(callback());
+};
 
-// // Ao chamar a função doingThings:
-// doingThings(wakeUp);
-// doingThings(breakfast);
-// doingThings(sleep);
+// Ao chamar a função doingThings:
+doingThings(wakeUp);
+doingThings(breakfast);
+doingThings(sleep);
 
-// // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com 
+// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com 
 
-// const employeesGenerator = (fullName) => {
-//   const email = fullName.toLowerCase().split(' ').join('_');
-//   return {fullName, email: `${email}@trybe.com`};
-// }; // Nessa função como pede o retorno do nome completo com o e-mail da pessoa (sendo ele o nome completo), foi usado o toLowercase, split e join no parâmetro fullName.
+const employeesGenerator = (fullName) => {
+  const email = fullName.toLowerCase().split(' ').join('_');
+  return {fullName, email: `${email}@trybe.com`};
+}; // Nessa função como pede o retorno do nome completo com o e-mail da pessoa (sendo ele o nome completo), foi usado o toLowercase, split e join no fullName.
 
-// const newEmployees = (callback) => {
-//   const employees = {
-//     id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-//     id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-//     id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-//   }
-//   return employees;
-// };
-// console.log(newEmployees(employeesGenerator));
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+console.log(newEmployees(employeesGenerator));
 
-// // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
 const sorteio = (numeroSorteado) => {
     numeroSorteado = Math.floor((Math.random() *5) +1);
@@ -55,8 +55,31 @@ const compareNumbers = (meuNumero, callback) => {
   }
     return('Tente novamente');
 };
-// console.log(sorteio());
 console.log(compareNumbers(4, sorteio));
 
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 
+const notes = (rightAnswers, studentAnswers) => {
+  let count = 0;
+  for (let i = 0; i < rightAnswers.length; i += 1) {
+    const rightNotes = rightAnswers[i];
+    if (rightNotes === studentAnswers[i]) {
+      count += 1
+    } else if (studentAnswers[i] === 'N.A') {
+    count += 0
+  } else {
+    count -= 0.5
+  }
+}
+return count
+};
+
+const testResult = (rightAnswers, studentAnswers, callback) => {
+  return callback(rightAnswers, studentAnswers);
+}
+console.log(testResult(RIGHT_ANSWERS, STUDENT_ANSWERS, notes));
