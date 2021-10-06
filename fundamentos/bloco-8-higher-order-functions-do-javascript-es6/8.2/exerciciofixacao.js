cc605482adbc12b9b0c8edb16061c2bdc90d88d8
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-10-06 00:06:45 -0300

    Adiciona atualizaçoes

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.2/exerciciofixacao.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.2/exerciciofixacao.js
@@ -1,13 +1,16 @@
+// FIND:
+
 // 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
 
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+const findDivisibleBy3And5 = () =>
+  numbers.find((number) => number % 3 === 0 && number % 5 === 0);
 console.log(findDivisibleBy3And5());
 
 // 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
 
-const names = ['João', 'Irene', 'Fernando', 'Maria'];
+const names = ["João", "Irene", "Fernando", "Maria"];
 
 const findNameWithFiveLetters = () => names.find((name) => name.length === 5);
 console.log(findNameWithFiveLetters());
@@ -15,17 +18,45 @@ console.log(findNameWithFiveLetters());
 // 3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
 
 const musicas = [
-  { id: '31031685', title: 'Partita in C moll BWV 997' },
-  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
-  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
-]
+  { id: "31031685", title: "Partita in C moll BWV 997" },
+  { id: "31031686", title: "Toccata and Fugue, BWV 565" },
+  { id: "31031687", title: "Chaconne, Partita No. 2 BWV 1004" },
+];
 
 // console.log(musicas[0].title);
 
-  function findMusic(id) {
-    const objmusic = musicas.find((musica) => (musica.id === id));
-    const title = objmusic.title;
-    return title;
-  };
+function findMusic(id) {
+  const objmusic = musicas.find((musica) => musica.id === id);
+  const title = objmusic.title;
+  return title;
+}
 
-console.log(findMusic('31031685'));
+console.log(findMusic("31031685"));
+
+// SOME E EVERY:
+
+// 1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+
+const names = ["Mateus", "José", "Ana", "Cláudia", "Bruna"];
+
+const hasName = (arr, name) => {
+  return arr.some((value) => value === name);
+};
+console.log(hasName(names, "Ana"));
+
+// 2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((person) => person <= minimumAge);
+};
+
+console.log(verifyAges(people, 18));
