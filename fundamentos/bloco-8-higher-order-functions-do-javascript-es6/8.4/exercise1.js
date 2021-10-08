fa536932b4fb057b8f0c374f4496c9312df80bc8
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-10-08 00:32:47 -0300

    Adiciona exercicio 1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.4/exercise1.js
@@ -0,0 +1,12 @@
+// 1 - Dada uma matriz, transforme em um array.
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acc, crr) => acc.concat(crr));
+};
+console.log(flatten());
