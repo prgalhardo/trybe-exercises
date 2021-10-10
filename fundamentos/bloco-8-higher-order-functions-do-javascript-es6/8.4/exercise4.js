bd2661de18505b1b68154557b0550b3d232ccd8b
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-10-10 17:21:24 -0300

    Exercicio 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.4/exercise4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/8.4/exercise4.js
@@ -1,60 +1,60 @@
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -63,18 +63,24 @@ const books = [
 
 //4. Encontre o livro com o maior nome.
 
-
 const expectedResult = {
   id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  genre: 'Fantasia',
+  name: "As Crônicas de Gelo e Fogo",
+  genre: "Fantasia",
   author: {
-    name: 'George R. R. Martin',
+    name: "George R. R. Martin",
     birthYear: 1948,
   },
   releaseYear: 1991,
 };
 
 function longestNamedBook() {
-  // escreva seu código aqui
+  // Usar o reduce para encontrar o objeto, dentro de uma varável de objetos, que possui o título do livro mais longo.
+  return books.reduce((acc, currentNameBook) => {
+    if (currentNameBook.name.length > acc.name.length) {
+      return currentNameBook;
+    }
+    return acc;
+  });
 }
+console.log(longestNamedBook());
